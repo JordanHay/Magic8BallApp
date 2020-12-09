41900901b0531a9a64823faf010bdc6879796d7d
JordanHay <51302288+JordanHay@users.noreply.github.com>
2020-12-09 09:37:51 -0500

    Add files via upload

---
+++ Magic8BallApp/Magic8Ball/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace Magic8Ball
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Image = global::Magic8Ball.Properties.Resources._15;
+            this.pictureBox.ImageLocation = "";
+            this.pictureBox.Location = new System.Drawing.Point(119, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(350, 350);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(119, 368);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(350, 20);
+            this.textBox.TabIndex = 1;
+            this.textBox.Text = "Ask your question";
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(252, 394);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(100, 30);
+            this.button.TabIndex = 2;
+            this.button.Text = "Ask";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button button;
+    }
+}
+
---
+++ Magic8BallApp/Magic8Ball/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Magic8Ball
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+
+            Random rnd = new Random();
+            int num = rnd.Next(1, 15);
+
+            switch(num)
+            {
+                case 1:
+                    pictureBox.Image = Properties.Resources._1;
+                    break;
+                case 2:
+                    pictureBox.Image = Properties.Resources._2;
+                    break;
+                case 3:
+                    pictureBox.Image = Properties.Resources._3;
+                    break;
+                case 4:
+                    pictureBox.Image = Properties.Resources._4;
+                    break;
+                case 5:
+                    pictureBox.Image = Properties.Resources._5;
+                    break;
+                case 6:
+                    pictureBox.Image = Properties.Resources._6;
+                    break;
+                case 7:
+                    pictureBox.Image = Properties.Resources._7;
+                    break;
+                case 8:
+                    pictureBox.Image = Properties.Resources._8;
+                    break;
+                case 9:
+                    pictureBox.Image = Properties.Resources._9;
+                    break;
+                case 10:
+                    pictureBox.Image = Properties.Resources._10;
+                    break;
+                case 11:
+                    pictureBox.Image = Properties.Resources._11;
+                    break;
+                case 12:
+                    pictureBox.Image = Properties.Resources._12;
+                    break;
+                case 13:
+                    pictureBox.Image = Properties.Resources._13;
+                    break;
+                case 14:
+                    pictureBox.Image = Properties.Resources._14;
+                    break;
+                case 15:
+                    pictureBox.Image = Properties.Resources._15;
+                    break;
+
+
+            }
+
+        }
+    }
+}
